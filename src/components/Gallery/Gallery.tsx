9aedd452885b306ea599559ff088a2ae0ca95d19
Ivaylo Georgiev <ivailo1411@gmail.com>
2025-11-02 15:20:54 +0200

    feat: adding gallery and some example images

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import { Flex, Title } from '@mantine/core';
+import { ColumnsPhotoAlbum } from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+
+import { galleryPhotos } from '../../constants/galleryPhotos';
+import 'react-photo-album/columns.css';
+import 'yet-another-react-lightbox/styles.css';
+import { Zoom } from 'yet-another-react-lightbox/plugins';
 
 const Gallery = () => {
+  const [index, setIndex] = useState<number>(-1);
+
   return (
     <Flex
       px='lg'
@@ -12,9 +22,36 @@ const Gallery = () => {
       <Title
         c='ocean.5'
         component='h2'
-        fw={700}>
+        fw={700}
+        mb='xl'>
         Gallery
       </Title>
+      <ColumnsPhotoAlbum
+        photos={galleryPhotos}
+        padding={0}
+        spacing={10}
+        columns={4}
+        onClick={({ index: current }) => setIndex(current)}
+      />
+      <Lightbox
+        index={index}
+        slides={galleryPhotos}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        plugins={[Zoom]}
+        animation={{ zoom: 400 }}
+        zoom={{
+          maxZoomPixelRatio: 1,
+          zoomInMultiplier: 2,
+          doubleTapDelay: 300,
+          doubleClickDelay: 300,
+          doubleClickMaxStops: 2,
+          keyboardMoveDistance: 100,
+          wheelZoomDistanceFactor: 100,
+          pinchZoomDistanceFactor: 100,
+          scrollToZoom: true,
+        }}
+      />
     </Flex>
   );
 };
